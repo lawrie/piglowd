3228cf88eac58c57ddb949719e15355d1c617ed5
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2014-09-22 09:22:54 +0100

    Tidied code

--- piglowd.c
+++ piglowd.c
@@ -42,6 +42,7 @@ struct instruction {
 static int pattern = -1, fd, lt, np=0, is_daemon = FALSE, verbose=FALSE, stepping=FALSE;
 static char**tokens;
 
+// Test for all digits
 static int is_numeric(char *str)
 {
   while(*str)
@@ -50,9 +51,11 @@ static int is_numeric(char *str)
       return 0;
     str++;
   }
-
+  
   return 1;
 }
+
+// Open the input fifo
 static int open_fifo(void) 
 {
   char fifo_name[] = "/tmp/piglowfifo";
@@ -70,6 +73,7 @@ static int open_fifo(void)
   return fd;
 }
 
+// Read a single character command or a pattern number from the FIFO
 static int read_fifo(void) {
   char buf[1];
 
@@ -83,6 +87,7 @@ static int read_fifo(void) {
   else return -1;
 }
 
+// Send an error message to the daemon log or console
 static void error(char *msg, ...)
 {
   char str[100];
@@ -97,6 +102,7 @@ static void error(char *msg, ...)
   else printf(str);
 }
 
+// Log an information message to the daemoon log or console
 static void log_msg(char *msg, ...)
 { 
   char str[100];
@@ -111,85 +117,88 @@ static void log_msg(char *msg, ...)
   else printf(str);
 }
 
+// Become a daemon
 static void become_daemon()
 {
-    pid_t pid;
-    int pidFilehandle;
-    char* pidfile = "/tmp/piglowd.pid";
-    char str[10];
+  pid_t pid;
+  int pidFilehandle;
+  char* pidfile = "/tmp/piglowd.pid";
+  char str[10];
 
-    /* Fork off the parent process */
-    pid = fork();
+  /* Fork off the parent process */
+  pid = fork();
 
-    /* An error occurred */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
+  /* An error occurred */
+  if (pid < 0)
+    exit(EXIT_FAILURE);
 
-    /* Success: Let the parent terminate */
-    if (pid > 0)
-        exit(EXIT_SUCCESS);
+  /* Success: Let the parent terminate */
+  if (pid > 0)
+    exit(EXIT_SUCCESS);
 
-    /* On success: The child process becomes session leader */
-    if (setsid() < 0)
-        exit(EXIT_FAILURE);
+  /* On success: The child process becomes session leader */
+  if (setsid() < 0)
+    exit(EXIT_FAILURE);
 
-    /* Catch, ignore and handle signals */
-    //TODO: Implement a working signal handler */
-    signal(SIGCHLD, SIG_IGN);
-    signal(SIGHUP, SIG_IGN);
+  /* Catch, ignore and handle signals */
+  //TODO: Implement a working signal handler */
+  signal(SIGCHLD, SIG_IGN);
+  signal(SIGHUP, SIG_IGN);
 
-    /* Fork off for the second time*/
-    pid = fork();
+  /* Fork off for the second time*/
+  pid = fork();
 
-    /* An error occurred */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
+  /* An error occurred */
+  if (pid < 0)
+    exit(EXIT_FAILURE);
 
-    /* Success: Let the parent terminate */
-    if (pid > 0)
-        exit(EXIT_SUCCESS);
+  /* Success: Let the parent terminate */
+  if (pid > 0)
+    exit(EXIT_SUCCESS);
 
-    /* Set new file permissions */
-    umask(0);
+  /* Set new file permissions */
+  umask(0);
 
-    /* Change the working directory to the root directory */
-    /* or another appropriated directory */
-    chdir("/");
+  /* Change the working directory to the root directory */
+  /* or another appropriated directory */
+  chdir("/");
 
-    /* Close all open file descriptors */
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
+  /* Close all open file descriptors */
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
 
-    /* Open the log file */
-    openlog ("pilogd", LOG_PID, LOG_DAEMON);
+  /* Open the log file */
+  openlog ("pilogd", LOG_PID, LOG_DAEMON);
+  
+  is_daemon = TRUE;
 
-    /* Ensure only one copy */
-    pidFilehandle = open(pidfile, O_RDWR|O_CREAT, 0600);
+  /* Ensure only one copy */
+  pidFilehandle = open(pidfile, O_RDWR|O_CREAT, 0600);
  
-    if (pidFilehandle == -1 )
-    {
-      /* Couldn't open lock file */
-      log_msg("Could not open PID lock file %s, exiting", pidfile);
-      exit(EXIT_FAILURE);
-    }
-    /* Try to lock file */
-    if (lockf(pidFilehandle,F_TLOCK,0) == -1)
-    {
-      /* Couldn't get lock on lock file */
-      log_msg("Could not lock PID lock file %s, exiting", pidfile);
-      exit(EXIT_FAILURE);
-    }
+  if (pidFilehandle == -1 )
+  {
+    /* Couldn't open lock file */
+    log_msg("Could not open PID lock file %s, exiting", pidfile);
+    exit(EXIT_FAILURE);
+  }
+  
+  /* Try to lock file */
+  if (lockf(pidFilehandle,F_TLOCK,0) == -1)
+  {
+    /* Couldn't get lock on lock file */
+    log_msg("Could not lock PID lock file %s, exiting", pidfile);
+    exit(EXIT_FAILURE);
+  }
  
-    /* Get and format PID */
-    sprintf(str,"%d\n",getpid());
+  /* Get and format PID */
+  sprintf(str,"%d\n",getpid());
  
-    /* write pid to lockfile */
-    write(pidFilehandle, str, strlen(str));
-
-    is_daemon = TRUE;
+  /* write pid to lockfile */
+  write(pidFilehandle, str, strlen(str));
 }
 
+// Read the patterns from the config file
 static char** read_config(void)
 {
   int lines_allocated = 5;
@@ -200,14 +209,14 @@ static char** read_config(void)
   if (words==NULL)
   {
     fprintf(stderr,"Out of memory (1).\n");
-    exit(1);
+    exit(EXIT_FAILURE);
   }
 
   FILE *fp = fopen("/etc/piglowd/piglowd.conf", "r");
   if (fp == NULL)
   {
     fprintf(stderr,"Error opening file.\n");
-    exit(2);
+    exit(EXIT_FAILURE);
   }
 
   int i;
@@ -227,16 +236,18 @@ static char** read_config(void)
       if (words==NULL)
       {
         fprintf(stderr,"Out of memory.\n");
-        exit(3);
+        exit(EXIT_FAILURE);
       }
       lines_allocated = new_size;
     }
+	
     /* Allocate space for the next line */
     words[i] = malloc(max_line_len);
+	
     if (words[i]==NULL)
     {
       fprintf(stderr,"Out of memory (3).\n");
-      exit(4);
+      exit(EXIT_FAILURE);
     }
     for(;;) 
     {
@@ -266,54 +277,56 @@ static char** read_config(void)
   return words;
 }
 
+// Split a line into tokens
 static char** str_split(char* a_str, const char a_delim)
 {
-    char** result    = 0;
-    size_t count     = 0;
-    char* tmp        = a_str;
-    char* last_delim = 0;
-    char delim[2];
-    delim[0] = a_delim;
-    delim[1] = 0;
-    char *save_ptr;
+  char** result    = 0;
+  size_t count     = 0;
+  char* tmp        = a_str;
+  char* last_delim = 0;
+  char delim[2];
+  delim[0] = a_delim;
+  delim[1] = 0;
+  char *save_ptr;
 
-    /* Count how many elements will be extracted. */
-    while (*tmp)
+  /* Count how many elements will be extracted. */
+  while (*tmp)
+  {
+    if (a_delim == *tmp)
     {
-        if (a_delim == *tmp)
-        {
-            count++;
-            last_delim = tmp;
-        }
+      count++;
+      last_delim = tmp;
+    }
         tmp++;
-    }
+  }
 
-    /* Add space for trailing token. */
-    count += last_delim < (a_str + strlen(a_str) - 1);
+  /* Add space for trailing token. */
+  count += last_delim < (a_str + strlen(a_str) - 1);
 
-    /* Add space for terminating null string so caller
-       knows where the list of returned strings ends. */
-    count++;
+  /* Add space for terminating null string so caller
+     knows where the list of returned strings ends. */
+  count++;
 
-    result = malloc(sizeof(char*) * count);
+  result = malloc(sizeof(char*) * count);
 
-    if (result)
+  if (result)
+  {
+    size_t idx  = 0;
+    char* token = strtok_r(a_str, delim, &save_ptr);
+
+    while (token)
     {
-        size_t idx  = 0;
-        char* token = strtok_r(a_str, delim, &save_ptr);
-
-        while (token)
-        {
-            assert(idx < count);
-            *(result + idx++) = strdup(token);
-            token = strtok_r(0, delim, &save_ptr);
-        }
-        assert(idx == count - 1);
-        *(result + idx) = 0;
+      assert(idx < count);
+      *(result + idx++) = strdup(token);
+      token = strtok_r(0, delim, &save_ptr);
     }
-    return result;
+    assert(idx == count - 1);
+    *(result + idx) = 0;
+  }
+  return result;
 }
 
+// Switch off all the LEDs
 static void clear(void) 
 {
   int i;
@@ -321,6 +334,7 @@ static void clear(void)
   for(i=0;i<3;i++) piGlowLeg(i,0);
 }
 
+// Execute compiled instructions
 static int execute(struct instruction *instructions, int level, int start, int end, int* index){
   int i, j, e;
 
@@ -386,7 +400,8 @@ static int execute(struct instruction *instructions, int level, int start, int e
   return TRUE;
 }
 
-struct instruction * compile(char * pattern) 
+// Compile a pattern
+struct instruction *compile(char * pattern) 
 {
   char* pattern_copy = strdup(pattern); 
   tokens = str_split(pattern_copy,' ');
@@ -437,7 +452,7 @@ struct instruction * compile(char * pattern)
         end = args[1];
       }
 	    
-      if (*token == 'l') 
+      if (*token == 'l') // Leg
       {
         if (strlen(token) < 4 || token[2] != '=') 
         {
@@ -450,33 +465,33 @@ struct instruction * compile(char * pattern)
           error("Invalid leg value: %s\n",token);
           return NULL;
         }
-	instructions[i].opcode = OPCODE_LEG;
-	instructions[i].p1 = leg;
-	instructions[i].p2 =  ((right[0] >= 'i'  && right[0] <= 'k') ? (right[0] - 'h') * -1 : atoi(right));
+        instructions[i].opcode = OPCODE_LEG;
+        instructions[i].p1 = leg;
+        instructions[i].p2 =  ((right[0] >= 'i'  && right[0] <= 'k') ? (right[0] - 'h') * -1 : atoi(right));
         if (instructions[i].p2 < -3 || instructions[i].p2 > 255) 
         {
           error("Invalid intensity value: %s\n",token);
           return NULL;
         }
       }
-      else if (*token == 'r')
+      else if (*token == 'r') // Ring
       {
         if (strlen(token) < 4 || (token[2] != '=' && token[2] != 'l')) 
         {
           error("Invalid ring token: %s\n",token);
           return NULL;
         }
-	ring = ((token[1] >= 'i'  && token[1] <= 'k') ? (token[1] - 'h') * -1 : token[1] - '0');
+        ring = ((token[1] >= 'i'  && token[1] <= 'k') ? (token[1] - 'h') * -1 : token[1] - '0');
         if (ring < -3 || ring > 5) 
         {
           error("Invalid ring value: %s\n", token);
           return NULL;
         }
-	if(strlen(left) == 2)
-	{
+        if(strlen(left) == 2) // Ring only
+        {
        	  instructions[i].opcode = OPCODE_RING;
           instructions[i].p1 = ring;
-	  instructions[i].p2 = ((right[0] >= 'i'  && right[0] <= 'k') ? (right[0] - 'h') * -1 : atoi(right));
+	      instructions[i].p2 = ((right[0] >= 'i'  && right[0] <= 'k') ? (right[0] - 'h') * -1 : atoi(right));
           if (instructions[i].p2 < -3 || instructions[i].p2 > 255) 
           {
             error("Invalid intensity value: %s\n", token);
@@ -484,41 +499,41 @@ struct instruction * compile(char * pattern)
           }
           if (ring < 0) fprintf(fp, "%s  PiGlowRing(%c %c 6,%d)\n", indent, token[1], '%',instructions[i].p2);
           else fprintf(fp,"    piGlowRing(%d,%d)\n", ring, instructions[i].p2);
-	}
-	else if (strlen(left) == 4)
+        }
+        else if (strlen(left) == 4) // Ring and leg
         {
-	  leg = ((token[3] >= 'i' && token[3] <= 'k') ? (token[3] - 'h') * -1 : token[3] - '0');
+	      leg = ((token[3] >= 'i' && token[3] <= 'k') ? (token[3] - 'h') * -1 : token[3] - '0');
           if (leg < -3 || leg > 2) 
           {
             error("Invalid leg value: %s\n", token);
             return NULL;
           }
           instructions[i].opcode = OPCODE_LED;
-	  instructions[i].p1 = leg;
-	  instructions[i].p2 = ring;
-	  instructions[i].p3 = ((right[0] >= 'i'  && right[0] <= 'k') ? (right[0] - 'h') * -1 : atoi(right));
+	      instructions[i].p1 = leg;
+	      instructions[i].p2 = ring;
+	      instructions[i].p3 = ((right[0] >= 'i'  && right[0] <= 'k') ? (right[0] - 'h') * -1 : atoi(right));
           if (instructions[i].p3 < -3 || instructions[i].p3 > 255) 
           {
             error("Invalid intensity value: %s\n", token);
             return NULL;
           }
-	}
+        }
         else 
         {
           error("Invalid ring token (left length): %s\n", token);
           return NULL;
         }
       } 
-      else if (*token >= 'i' && *token <= 'k')
+      else if (*token >= 'i' && *token <= 'k') // Variable
       {
-        if (token[1] == '?')
+        if (token[1] == '?') // Random value
         {
           instructions[i].opcode = OPCODE_RANDOM;
           instructions[i].p1 =  *token - 'i';
           instructions[i].p2 = atoi(start);
           instructions[i].p3 = atoi(end);
         } 
-        else if (token[1] == ':')
+        else if (token[1] == ':') // Assignment (subtraction)
         {
           instructions[i].opcode = OPCODE_ASSIGN;
           instructions[i].p1 =  *token - 'i';
@@ -527,7 +542,7 @@ struct instruction * compile(char * pattern)
         } 
         else
         {
-          if (token[1] != '=') 
+          if (token[1] != '=') // For loop
           {
             error("Invalid loop token: %s\n", token);
             return NULL;
@@ -558,7 +573,7 @@ struct instruction * compile(char * pattern)
           fprintf(fp,"%s  {\n",indent);
         }
       }
-      else if (*token == 'd')
+      else if (*token == 'd') // Delay
       {
         if (!is_numeric(&token[1]) && (strlen(token) > 2 || token[1] < 'i' || token[1] > 'k')) 
         {
@@ -583,6 +598,7 @@ struct instruction * compile(char * pattern)
   return instructions;
 }
 
+// Main entry point
 int main (int argc, char *argv[])
 {
   char** patterns;
